07255166a5662cb7ad76f9293a52cad9eb0ca41a
NoMoreSkill <dima111spring@gmail.com>
2023-01-10 23:29:57 +0400

    Добавьте файлы проекта.

---
+++ TestProj/StringChanger.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	const int C = 50;
+	string a;
+	char b[C];
+	cout << "Ввод строки:";
+	cin >> a;
+	int l = a.length();
+	transform(a.begin(), a.end(), a.begin(), tolower);//стандартная (почти) функция преобразования регистра символов, но работает ТОЛЬКО с англ буквами. С кириллицей не работает!!!!
+	for (int i = 0; i < l; i++)
+		for (int j = 0; j < l; j++)
+		{
+			if (a[i] == a[j] && i != j)
+				b[i] = ')';
+			if ((j + 1) == l && b[i] != ')')
+				b[i] = '(';
+		}
+	for (int i = 0; i < l; i++)
+		cout << b[i];
+	return 0;
+}
